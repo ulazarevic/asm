dd1193471e7dc735bf07db3522fdac429e55f213
Uros <uxa.lazarevic@gmail.com>
2018-02-11 02:57:30 +0100

    new methods

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ def read_file(fileName,rangeEnd, columnName, columnYear, collabGraph, professors
         for char in removableChars:
             authors = authors.replace(char,'')
         authors = authors.split(',')
-        #allNative = True
         authorsCopy = [i for i in authors]
         for author in authors:
             if not (author in professorsDict):
@@ -45,6 +44,34 @@ def read_file(fileName,rangeEnd, columnName, columnYear, collabGraph, professors
                 collabGraph[m][n] = collabGraph[m][n] + 1
                 collabGraph[n][m] = collabGraph[n][m] + 1
 
+def num_of_zeros(array):
+    num = 0
+    for item in array:
+        if item == 0:
+            num += 1
+    return num
+
+def average_coauthors(collabGraph):
+    average_array = [0] * collabGraph.shape[0]
+    index = 0
+    for row in collabGraph:
+        if len(row) - num_of_zeros(row) == 0:
+            continue
+        average_array[index] = (sum(row)/(len(row) - num_of_zeros(row)))
+        index += 1
+    return average_array
+
+def collab_departments(collabGraph, professorsList):
+    num_different = 0
+    num_same = 0
+    size = collabGraph.shape[0]
+    for i in range(0, size):
+        for j in range(0, size):
+            if (professorsList[i]['department'] != professorsList[j]['department']) and (collabGraph[i][j] > 0):
+                num_different += 1
+            elif (professorsList[i]['department'] == professorsList[j]['department']) and (collabGraph[i][j] > 0):
+                num_same += 1
+    return {'same':num_same, 'different':num_different}
 
 gexf = etree.Element('gexf', xmlns ='http://www.gexf.net/1.2draft', version='1.2')
 graph = etree.SubElement(gexf, 'graph', mode='static', defaultedgetype='undirected')
@@ -61,11 +88,13 @@ professors = openpyxl.load_workbook(os.path.join(consts.PROFESSORS_FILE_PATH))
 professorsSheet = professors.active
 print('Professors file reading...')
 professorsDict = {}
+professorsList = []
 
-for i in range (2, 254):
+for i in range (2, 255):
     katedra = cyrtranslit.to_latin(professorsSheet['A' + str(i)].value)
     ime = cyrtranslit.to_latin(professorsSheet['D' + str(i)].value)
     professorsDict[ime] = {ime: katedra, 'id': i-2}
+    professorsList.append({'name': ime, 'department': katedra})
     node = etree.SubElement(nodes, 'node', id= str(i-2), label = ime)
     attvalues = etree.SubElement(node, 'attvalues')
     attvalueDepartment = etree.SubElement(attvalues,'attvalue',value=katedra)
@@ -77,7 +106,8 @@ read_file(consts.PROFESSORS_NATIVE_PATH, 844,'M', 'L', collabGraph, professorsDi
 read_file(consts.PROFESSORS_PAPERS_PATH, 2317, 'B', 'H', collabGraph, professorsDict)
 read_file(consts.PROFESSORS_INTERNATIONAL_PATH, 887, 'M', 'L', collabGraph, professorsDict)
 
-
+average_coauthors(collabGraph)
+print(collab_departments(collabGraph, professorsList))
 
 write_to_file(gexf,'gephidata.gexf')
 
